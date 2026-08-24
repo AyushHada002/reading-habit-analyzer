3e4de5cb783a368deb1a874776dd57955d0f22ed
AyushHada002 <ayushhada002@gmail.com>
2026-08-24 13:45:33 +0530

    chore: initialize Spring Boot project

---
+++ reading-habit-analyzer/src/main/java/com/ayush/readinghabit/ReadingHabitAnalyzerApplication.java
@@ -0,0 +1,13 @@
+package com.ayush.readinghabit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReadingHabitAnalyzerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReadingHabitAnalyzerApplication.class, args);
+	}
+
+}
---
+++ reading-habit-analyzer/src/test/java/com/ayush/readinghabit/ReadingHabitAnalyzerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ayush.readinghabit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReadingHabitAnalyzerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
